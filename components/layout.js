c065ad99c5de27350d72772456e659f839bfe220
Sanhanat-Somnee-66026189 <150765721+Sanhanat-Somnee-66026189@users.noreply.github.com>
2024-11-26 16:51:27 +0700

    Update layout.js

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Sanhanat Somnee';
+const name = 'Sanhanat Somnee Test';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
